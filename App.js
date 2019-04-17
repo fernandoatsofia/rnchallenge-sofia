20ee43c60be88c2216d3e88d443fc7a6458a40ff
José Miguel Gutierrez Delgado <jmgtzd1995@gmail.com>
2019-04-16 20:09:23 -0500

    Sofia challenge

--- App.js
+++ App.js
@@ -1,49 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import * as React from 'react';
+import {Text, View, StyleSheet, Alert} from 'react-native';
+import {Constants} from 'expo';
+import PropTypes from "prop-types";
+import axios from "axios";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const API_URL = 'https://jsonplaceholder.typicode.com/users'
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+class ItemList extends React.Component {
+    render() {
+        const {username, email} = this.props;
+        return (
+            <Text> El usuario {username} tiene el mail {email} </Text>
+        )
+    }
+
+}
+
+
+class ItemsList extends React.Component {
+
+    static propTypes = {
+        userData: PropTypes.array
+    }
+
+    static defaultProps = {
+        userData: []
+    }
+
+    renderOrderedList() {
+        const _orderedData = [];
+        this.props.userData.map((item, index) => {
+            _orderedData.push(item.username);
+        });
+        _orderedData.sort();
+
+        return _orderedData.map((item) => {
+            return (<Text>{item}</Text>)
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                {this.props.userData.map((item, index) => {
+                    return (<ItemList username={item.username} email={item.email}/>)
+                })}
+                <Text> Lista ordenada </Text>
+                {this.renderOrderedList()}
+
+            </View>
+        )
+    }
+}
+
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            usersData: []
+        }
+    }
+
+    componentDidMount() {
+        this.fetchUsersData();
+    }
+
+    fetchUsersData() {
+        axios.get(API_URL)
+            .then((response) => {
+                // handle success
+                this.setState({
+                    usersData: response.data
+                });
+                // console.log(response.data);
+            })
+            .catch((error) => {
+                // handle error
+                Alert.alert("Error", "Ha ocurrido un error")
+                console.log(error);
+            })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    {this.state.usersData && <ItemsList userData={this.state.usersData}/>}
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+        padding: 8,
+    },
 });
